aba8b16ceaedd0a5b2c90873f47c9a8b4fb51687
Suvethagan <36789064+Suvethagan@users.noreply.github.com>
2018-02-24 16:42:16 +0530

    Fc

---
+++ perimeter/PerimeterAreaSquare.java
@@ -0,0 +1,9 @@
+public class PerimeterAreaSquare{
+	public static void main(String args[]){
+		int side1 = 150;
+		int perimeter = 2* (side1 + side1);
+			System.out.println(perimeter);
+		int area = side1 * side1;
+			System.out.println(area);
+}
+}
