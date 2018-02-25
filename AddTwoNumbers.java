5794a2a9915896b5bc2b09341839432f83e5ae92
Suvethagan <36789064+Suvethagan@users.noreply.github.com>
2018-02-25 20:47:17 +0530

    fc
    eclipse

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,9 +1,9 @@
-public class AddTwoNumbers{
-	public static void main(String args[]){
-		int num1 = 234;
-		int num2 = 567;
-		int result = num1 + num2;
-		System.out.println(result);
-		//System.out.println("Sum of " + num1 + " + " + num2 + " = " + result);
+
+public class AddTwoNumbers {
+	public static void main(String args[]) {
+		int num1 =100;
+		int num2 =200;
+		int sum = num1 + num2;
+		System.out.println(sum);
 	}
 }
